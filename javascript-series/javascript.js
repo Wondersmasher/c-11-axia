61a35e99c35dac04139f7438eccee11cd6d00881
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2026-06-30 08:47:22 +0100

    Add TypeScript examples for variable types, utility types, and user profiles

--- javascript-series/javascript.js
+++ javascript-series/javascript.js
@@ -1,161 +1,165 @@
-console.log("Hello World!");
+// console.log("Hello World!");
 
-// var => legacy syntax
-// let => can change over time
-let name = "John";
-// console.log(name, "name 1");
+// // var => legacy syntax
+// // let => can change over time
+// let name = "John";
+// // console.log(name, "name 1");
 
-name = "Jane";
-// console.log(name, "name 2");
+// name = "Jane";
+// // console.log(name, "name 2");
 
-// const => cannot change over the entire lifetime of that variable
-const name2 = "John";
-// console.log(name2, "name 3");
+// // const => cannot change over the entire lifetime of that variable
+// const name2 = "John";
+// // console.log(name2, "name 3");
 
-// string
-const firstName = "John";
-// number
-const age = 30;
-// boolean
-const hasKids = true;
-// null
-const car = null;
-// undefined
-let test;
-// symbol
-const symbol1 = Symbol();
-const symbol2 = Symbol();
-// object
+// // string
+// const firstName = "John";
+// // number
+// const age = 30;
+// // boolean
+// const hasKids = true;
+// // null
+// const car = null;
+// // undefined
+// let test;
+// // symbol
+// const symbol1 = Symbol();
+// const symbol2 = Symbol();
+// // object
 
-// console.log(firstName, age, hasKids, car, test, symbol1, symbol2);
+// // console.log(firstName, age, hasKids, car, test, symbol1, symbol2);
 
-// control flow
+// // control flow
 
-// if (age > 18) {
-//   console.log("You are over 18");
-// } else if (age > 18 && age < 30) {
-//   console.log("You are between 18 and 30");
-// } else {
-//   console.log("You are under 18");
+// // if (age > 18) {
+// //   console.log("You are over 18");
+// // } else if (age > 18 && age < 30) {
+// //   console.log("You are between 18 and 30");
+// // } else {
+// //   console.log("You are under 18");
+// // }
+
+// // switch (age) {
+// //   case 18:
+// //     console.log("You are 18");
+// //     break;
+// //   case 30:
+// //     console.log("You are 30");
+// //     break;
+// //   default:
+// //     console.log("You are not 18 or 30");
+// //     break;
+// // }
+
+// // FizzBuzz
+// // write a program that logs or returns numbers from 1 to 100.
+// // However, for multiples of 3, you output "Fizz";
+// // for multiples of 5, you output "Buzz";
+// // and for numbers that are multiples of both 3 and 5,
+// // you output "FizzBuzz".
+
+// // for (let i = 1; i <= 100; i++) {
+// //   const isFizz = i % 3 === 0;
+// //   const isBuzz = i % 5 === 0;
+// //   const isFizzBuzz = isFizz && isBuzz;
+
+// //   if (isFizzBuzz) console.log("FizzBuzz");
+// //   else if (isFizz) console.log("Fizz");
+// //   else if (isBuzz) console.log("Buzz");
+// //   else console.log(`the value is ${i}`);
+// // }
+// // for (let i = 1; i <= 100; i++) {
+// //   console.log(
+// //     `${
+// //       i % 3 === 0 && i % 5 === 0
+// //         ? `FizzBuzz`
+// //         : i % 3 === 0
+// //           ? "Fizz"
+// //           : i % 5 === 0
+// //             ? "Buzz"
+// //             : `the value is: ${i}`
+// //     }`,
+// //   );
+// // }
+
+// // Loops
+
+// // 1. Conventional for loop
+// for (let i = 0; i < 10; i++) {
+//   // console.log(i);
 // }
 
-// switch (age) {
-//   case 18:
-//     console.log("You are 18");
-//     break;
-//   case 30:
-//     console.log("You are 30");
-//     break;
-//   default:
-//     console.log("You are not 18 or 30");
-//     break;
+// for (let i = 10; i > 0; i--) {
+//   // console.log(i);
 // }
 
-// FizzBuzz
-// write a program that logs or returns numbers from 1 to 100.
-// However, for multiples of 3, you output "Fizz";
-// for multiples of 5, you output "Buzz";
-// and for numbers that are multiples of both 3 and 5,
-// you output "FizzBuzz".
+// // 2. For of loop
+// const names = ["John", "Jane", "Jack", "Jill"];
 
-// for (let i = 1; i <= 100; i++) {
-//   const isFizz = i % 3 === 0;
-//   const isBuzz = i % 5 === 0;
-//   const isFizzBuzz = isFizz && isBuzz;
-
-//   if (isFizzBuzz) console.log("FizzBuzz");
-//   else if (isFizz) console.log("Fizz");
-//   else if (isBuzz) console.log("Buzz");
-//   else console.log(`the value is ${i}`);
-// }
-// for (let i = 1; i <= 100; i++) {
-//   console.log(
-//     `${
-//       i % 3 === 0 && i % 5 === 0
-//         ? `FizzBuzz`
-//         : i % 3 === 0
-//           ? "Fizz"
-//           : i % 5 === 0
-//             ? "Buzz"
-//             : `the value is: ${i}`
-//     }`,
-//   );
+// for (const name of names) {
+//   // console.log(name);
 // }
 
-// Loops
+// // 3. For in loop
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 30,
+// };
 
-// 1. Conventional for loop
-for (let i = 0; i < 10; i++) {
-  // console.log(i);
-}
+// for (const key in person) {
+//   // console.log(`${key}: ${person[key]}`);
+// }
 
-for (let i = 10; i > 0; i--) {
-  // console.log(i);
-}
+// // 4. While loop
+// let i = 0;
+// while (i < 10) {
+//   // console.log(i);
+//   i++;
+// }
 
-// 2. For of loop
-const names = ["John", "Jane", "Jack", "Jill"];
+// // 5. Do while loop
+// let j = 0;
+// do {
+//   // console.log(j);
+//   j++;
+// } while (j < 10);
 
-for (const name of names) {
-  // console.log(name);
-}
+// // FUNCTIONS
+// // 1. Function declaration
+// greet("John");
+// function greet(name = "world!") {
+//   console.log(`Hello ${name}`);
+// }
+// // 2. Function expression
+// const greet2 = function (name = "default 2") {
+//   console.log(`Hello 2 ${name}`);
+// };
+// greet2();
 
-// 3. For in loop
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 30,
-};
+// // 3. Arrow function
+// const greet3 = (name = "default 3") => {
+//   console.log(`Hello 3 ${name}`);
+//   return `Hello 3: ${name}`;
+// };
+// const val = greet3();
+// console.log(val, "val");
+// greet();
 
-for (const key in person) {
-  // console.log(`${key}: ${person[key]}`);
-}
+// // 4. IIFE (Immediately Invoked Function Expression)
+// (function (name = "default 4") {
+//   console.log(`Hello 4 ${name}`);
+// })("John");
 
-// 4. While loop
-let i = 0;
-while (i < 10) {
-  // console.log(i);
-  i++;
-}
+// // Assessment: Write a function that takes in 2 numbers and consoles the sum of those numbers. If either of the numbers is not a number, return "Invalid input".
 
-// 5. Do while loop
-let j = 0;
-do {
-  // console.log(j);
-  j++;
-} while (j < 10);
-
-// FUNCTIONS
-// 1. Function declaration
-greet("John");
-function greet(name = "world!") {
-  console.log(`Hello ${name}`);
-}
-// 2. Function expression
-const greet2 = function (name = "default 2") {
-  console.log(`Hello 2 ${name}`);
-};
-greet2();
-
-// 3. Arrow function
-const greet3 = (name = "default 3") => {
-  console.log(`Hello 3 ${name}`);
-  return `Hello 3: ${name}`;
-};
-const val = greet3();
-console.log(val, "val");
-greet();
-
-// 4. IIFE (Immediately Invoked Function Expression)
-(function (name = "default 4") {
-  console.log(`Hello 4 ${name}`);
-})("John");
-
-// Assessment: Write a function that takes in 2 numbers and consoles the sum of those numbers. If either of the numbers is not a number, return "Invalid input".
-
-const addition = (num1, num2) => console.log(num1 + num2);
-
-addition(1, 4);
-addition(10, 20);
-addition(-10, 10);
-addition(1, "4"); // weird output because of type coercion.
+// // const addition = (num1, num2) =>{
+// console.log(num1 + num2);
+//   if (typeof num1 !== "number" || typeof num2 !== "number") {
+//     console.log("Invalid input");
+//   }
+// }
+// // addition(1, 4);
+// // addition(10, 20);
+// // addition(-10, 10);
+// // addition(1, "4"); // weird output because of type coercion.
--- javascript-series/javascript.ts
+++ javascript-series/javascript.ts
@@ -1,1 +1,291 @@
 console.log("Hello World!");
+
+let typescriptAge: number = 10;
+typescriptAge = 20;
+typescriptAge = 11;
+
+const typescriptName: string = "John";
+const age: number = 30;
+const hasKids: boolean = 8 < 10;
+
+type Num = number;
+
+const numNumber: Num = 10;
+
+type NumOrString = number | string;
+
+let num: NumOrString = 10;
+num = "10";
+
+type Status = "pending" | "approved" | "rejected" | "resolved";
+
+const pending: Status = "pending";
+
+const arr1: number[] = [1, 2, 3];
+const arr2: string[] = ["a", "b", "c"];
+const arr3: (string | number)[] = [1, "a", 2, "b", "c"];
+const arr4: (string | number | boolean)[] = [1, "a", 2, "b", "c", false];
+
+type Obj = {
+  name?: string;
+  age: number;
+};
+
+const obj1: Obj = {
+  name: "9",
+  age: 30,
+};
+
+interface Obj2 {
+  name?: string;
+  age: number;
+}
+
+const obj2: Obj2 = {
+  name: "9",
+  age: 30,
+};
+
+type BaseUserProfile = {
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
+type UserAddress = {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+};
+
+const user: BaseUserProfile = {
+  age: 30,
+  firstName: "John",
+  lastName: "Doe",
+};
+
+const userAddress: UserAddress = {
+  street: "123 Main St",
+  city: "Anytown",
+  state: "CA",
+  zip: "12345",
+};
+
+type CompleteUserProfile = BaseUserProfile & UserAddress;
+const userWithAddress: CompleteUserProfile = {
+  age: 30,
+  firstName: "John",
+  lastName: "Doe",
+  street: "123 Main St",
+  city: "Anytown",
+  state: "CA",
+  zip: "12345",
+};
+
+interface BaseUserProfileI {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+interface UserAddressI {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+}
+
+interface CompleteUserProfileI extends BaseUserProfileI, UserAddressI {}
+const userWithAddressI: CompleteUserProfileI = {
+  age: 30,
+  firstName: "John",
+  lastName: "Doe",
+  street: "123 Main St",
+  city: "Anytown",
+  state: "CA",
+  zip: "12345",
+};
+
+// Utility Types
+// 1. Partial
+// 2. Required
+// 3. Readonly
+// 4. Pick
+// 5. Omit
+// 6. Array
+// 7. Record
+
+// PARTIAL => makes all the fields optional
+
+type CompleteUserProfilePartialI = Partial<CompleteUserProfileI>;
+const userWithAddressPartialI: CompleteUserProfilePartialI = {
+  age: 30,
+  firstName: "John",
+  lastName: "Doe",
+  street: "123 Main St",
+  city: "Anytown",
+  state: "CA",
+  zip: "12345",
+};
+
+// REQUIRED => makes all the fields required
+type CompleteUserProfileRequiredI = Required<CompleteUserProfilePartialI>;
+const userWithAddressRequiredI: CompleteUserProfileRequiredI = {
+  age: 30,
+  firstName: "John",
+  lastName: "Doe",
+  street: "123 Main St",
+  city: "Anytown",
+  state: "CA",
+  zip: "12345",
+};
+
+// READONLY => makes all the fields readonly
+type CompleteUserProfileReadonlyI = Readonly<CompleteUserProfilePartialI>;
+const userWithAddressReadonlyI: CompleteUserProfileReadonlyI = {
+  //   age: 30,
+  //   firstName: "John",
+  lastName: "Doe",
+  street: "123 Main St",
+  city: "Anytown",
+  state: "CA",
+  zip: "12345",
+};
+
+// PICK => allows you to pick from a type
+type PickedUserDetails = Pick<
+  CompleteUserProfileRequiredI,
+  "firstName" | "lastName"
+>;
+
+// OMIT => allows you to omit from a type
+type OmittedUserDetails = Omit<
+  CompleteUserProfileRequiredI,
+  "firstName" | "lastName"
+>;
+
+// ARRAY => allows you to create an array of a specific type
+type ArrayOfNumbers = Array<number>;
+const arrayOfNumbers: ArrayOfNumbers = [1, 2, 3, 4, 5];
+type ArrayOfNumbersOrStrings = Array<number | string>;
+const arrayOfNumbersOrStrings: ArrayOfNumbersOrStrings = [1, "2", 3, "4", 5];
+type ArrayOfUserObjects = Array<CompleteUserProfileI>;
+const userObjects: ArrayOfUserObjects = [
+  {
+    age: 10,
+    city: "Anytown",
+    firstName: "John",
+    lastName: "Doe",
+    street: "123 Main St",
+    state: "CA",
+    zip: "12345",
+  },
+  {
+    age: 20,
+    city: "Anytown",
+    firstName: "John",
+    lastName: "James",
+    street: "123 Main St",
+    state: "CA",
+    zip: "12345",
+  },
+  {
+    age: 10,
+    city: "Anytown",
+    firstName: "John",
+    lastName: "Doe",
+    street: "123 Main St",
+    state: "CA",
+    zip: "12345",
+  },
+  {
+    age: 10,
+    city: "Anytown",
+    firstName: "John",
+    lastName: "Doe",
+    street: "123 Main St",
+    state: "CA",
+    zip: "12345",
+  },
+  {
+    age: 10,
+    city: "Anytown",
+    firstName: "John",
+    lastName: "Doe",
+    street: "123 Main St",
+    state: "CA",
+    zip: "12345",
+  },
+];
+
+// RECORD => allows you to create a record of a specific type
+type UserRecord = Record<string, CompleteUserProfileI>;
+const userRecord: UserRecord = {
+  "1": {
+    age: 10,
+    city: "Anytown",
+    firstName: "John",
+    lastName: "Doe",
+    street: "123 Main St",
+    state: "CA",
+    zip: "12345",
+  },
+  "2": {
+    age: 10,
+    city: "Anytown",
+    firstName: "John",
+    lastName: "Doe",
+    street: "123 Main St",
+    state: "CA",
+    zip: "12345",
+  },
+};
+
+// Assessment:
+// 1. Write a function that takes in 2 numbers and consoles the sum of those numbers. If either of the numbers is not a number, return "Invalid input".
+// 2. Write a function that takes an object and console log every key and value in that argument.
+// NOTE: YOU MUST USE TYPESCRIPT
+// TIME ALLOWED: 15 MINS
+
+// MY SOLUTION
+
+// 1.
+const addition = (num1: number, num2: number) => {
+  try {
+    console.log(num1 + num2);
+  } catch (error) {
+    console.log(`Invalid input with error: ${error}`);
+  }
+};
+const two: number = 2;
+
+addition(1, 2);
+addition(30, 99);
+// addition("30", 99);
+
+// 2.
+type ObjAssignment = Record<string, string>;
+
+const payload1: ObjAssignment = {
+  name: "John Doe",
+  street: "123 Main St",
+  city: "Anytown",
+  state: "CA",
+  zip: "12345",
+};
+const payload2: ObjAssignment = {
+  name: "John James",
+  street: "321 False St",
+  city: "This town",
+  state: "12345",
+  zip: "CA1234",
+};
+
+const printObj = (obj: ObjAssignment) => {
+  for (const key in obj) {
+    console.log(`${key}: ${obj[key]}`);
+  }
+};
+printObj(payload1);
+printObj(payload2);
